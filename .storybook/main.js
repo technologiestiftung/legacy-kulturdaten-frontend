8a43da5197141b4f438580f78541b24f9eb6d58c
Mathies Schneider <hello@mathies.io>
2021-03-23 10:49:31 +0100

    add and configure Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../components/**/story.tsx', '../components/**/*.story.tsx'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+};
---
+++ components/test/story.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+
+export default {
+  title: 'Test',
+};
+
+export const Test: Story = () => <h1>Test</h1>;
