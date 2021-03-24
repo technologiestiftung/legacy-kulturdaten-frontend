c1cec63b020b3fdab7391e164c4a343f49d7082d
Mathies Schneider <hello@mathies.io>
2021-03-24 17:48:51 +0100

    🎨 fix linting and prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,5 @@
-export default () => <h1>Hello Kulturdaten Frontend!</h1>;
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => <h1>Hello Kulturdaten Frontend!</h1>;
+
+export default IndexPage;
