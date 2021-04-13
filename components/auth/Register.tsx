fa10f97a35f4ff375034793e8d69b97eb058e663
Mathies Schneider <hello@mathies.io>
2021-04-13 15:51:10 +0200

    ✨ Add submitted state to register

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -7,15 +7,12 @@ export const RegisterForm: React.FC = () => {
   const [password, setPassword] = useState<string>('');
   const [passwordConfirmation, setPasswordConfirmation] = useState<string>('');
   const [error, setError] = useState<Error>();
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log({
-      email,
-      password,
-      passwordConfirmation,
-    });
+    setError(undefined);
 
     try {
       const resp = await call<AuthRegister>(
@@ -25,6 +22,7 @@ export const RegisterForm: React.FC = () => {
           password_confirmation: passwordConfirmation,
         })
       );
+      setSubmitted(true);
       console.log(resp);
     } catch (e) {
       setError(e);
@@ -34,49 +32,57 @@ export const RegisterForm: React.FC = () => {
 
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="register-email">email</label>
-          <br />
-          <input
-            value={email}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-            type="email"
-            id="register-email"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="register-password">password</label>
-          <br />
-          <input
-            value={password}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-            type="password"
-            id="register-password"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="register-password-confirmation">confirm password</label>
-          <br />
-          <input
-            value={passwordConfirmation}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPasswordConfirmation(e.target.value)}
-            type="password"
-            id="register-password-confirmation"
-            required
-          />
-        </div>
-        <div>
-          <input type="submit" value="register" />
-        </div>
-      </form>
+      {!submitted || error ? (
+        <form onSubmit={submitHandler}>
+          <div>
+            <label htmlFor="register-email">email</label>
+            <br />
+            <input
+              value={email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+              type="email"
+              id="register-email"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="register-password">password</label>
+            <br />
+            <input
+              value={password}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              type="password"
+              id="register-password"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="register-password-confirmation">confirm password</label>
+            <br />
+            <input
+              value={passwordConfirmation}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPasswordConfirmation(e.target.value)
+              }
+              type="password"
+              id="register-password-confirmation"
+              required
+            />
+          </div>
+          <div>
+            <input type="submit" value="register" />
+          </div>
+        </form>
+      ) : (
+        ''
+      )}
       {error ? (
         <div>
           <h3>{error.name}</h3>
           <pre>{error.message}</pre>
         </div>
+      ) : submitted ? (
+        <div>Form submitted. Please confirm your email address via the email we just sent you.</div>
       ) : (
         ''
       )}
