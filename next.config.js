58ba0689c87ed7bddab3670a4f2b045da7150af1
Mathies Schneider <hello@mathies.io>
2021-04-19 17:29:52 +0200

    ✨ Add locales

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 module.exports = {
   publicRuntimeConfig: {
-    defaultLocale: 'de-DE',
+    defaultLocale: 'catchAll',
     api: 'https://beta.api.kulturdaten.berlin',
     authTokenCookieName: 'AUTH_TOKEN',
   },
   trailingSlash: true,
+  i18n: {
+    locales: ['de-DE', 'en-DE', 'catchAll'],
+    defaultLocale: 'catchAll',
+  },
+  async redirects() {
+    console.log(this);
+    return [
+      {
+        source: '/catchAll',
+        destination: '/de-DE',
+        locale: false,
+        permanent: false,
+      },
+      {
+        source: '/catchAll/:slug*/',
+        destination: '/de-DE/:slug*/',
+        locale: false,
+        permanent: false,
+      },
+    ];
+  },
 };
