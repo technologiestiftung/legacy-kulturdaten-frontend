b4e6bba8b54145b0c66166975a4f24d0c6b1f0a4
Mathies Schneider <hello@mathies.io>
2021-05-04 09:14:52 +0200

    ✨ Make routing hooks storybook compatible

--- components/Navigation/MenuLink.tsx
+++ components/Navigation/MenuLink.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 
 import { ArrowRightSvg } from './ArrowRightSvg';
+import { useIsRouteStringActive } from '../../lib/routing';
 
 const StyledA = styled.a<{ active?: boolean }>`
   color: inherit;
@@ -27,10 +28,13 @@ export interface MenuLinkProps {
 }
 
 export const MenuLink: React.FC<MenuLinkProps> = ({ title, href, active }: MenuLinkProps) => {
+  const isRouteActive = useIsRouteStringActive(href);
+  const linkIsActive = active || isRouteActive;
+
   return (
     <Link href={href} passHref>
-      <StyledA title={title} active={active}>
-        {active ? <ArrowRightSvg /> : ''}
+      <StyledA title={title} active={linkIsActive}>
+        {linkIsActive ? <ArrowRightSvg /> : ''}
         {title}
       </StyledA>
     </Link>
