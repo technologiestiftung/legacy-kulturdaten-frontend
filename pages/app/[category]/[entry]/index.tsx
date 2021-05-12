510a3638aa6a9960aaa37fbfdb15be06dc32eba6
Mathies Schneider <hello@mathies.io>
2021-05-12 12:09:07 +0200

    ✨ Implement entry page PoC

--- pages/app/[category]/[entry]/index.tsx
+++ pages/app/[category]/[entry]/index.tsx
@@ -1,18 +1,93 @@
+import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import { contentGrid, mq } from '../../../../components/globals/Constants';
 import { TitleBar } from '../../../../components/navigation/TitleBar';
 import { AppWrapper } from '../../../../components/wrappers/AppWrapper';
-import { useT } from '../../../../lib/i18n';
+import { Categories, useCategories } from '../../../../config/categories';
+import { getApiUrlString, useApiCall } from '../../../../lib/api';
+import { Breakpoint } from '../../../../lib/WindowService';
+
+const EntryTitle = styled.h2`
+  font-size: var(--font-size-700);
+  line-height: var(--line-height-700);
+  font-weight: 700;
+`;
+const EntryDescription = styled.div`
+  padding-top: 1.5rem;
+  font-size: var(--font-size-600);
+  line-height: var(--line-height-600);
+
+  ${mq(Breakpoint.wide)} {
+    padding-top: 2.25rem;
+  }
+`;
+const EntryHead = styled.div`
+  grid-column: 1 / -1;
+  padding: 3.75rem 0.75rem;
+
+  ${mq(Breakpoint.mid)} {
+    padding: 6.75rem 0;
+    grid-column: 2 / span 4;
+  }
+`;
+const EntryContent = styled.div`
+  grid-column: 1 / -1;
+  padding: 0 0.75rem;
+
+  ${mq(Breakpoint.mid)} {
+    padding: 0;
+    grid-column: 2 / -2;
+  }
+`;
+const EntryContainer = styled.div`
+  ${contentGrid(4)}
+  ${mq(Breakpoint.mid)} {
+    ${contentGrid(11)}
+  }
+
+  ${mq(Breakpoint.wide)} {
+    ${contentGrid(10)}
+  }
+`;
 
 const EntryIndexPage: NextPage = () => {
   const router = useRouter();
-  const t = useT();
+  const categories = useCategories();
+  const call = useApiCall();
 
-  const title = router?.query?.category
-    ? (t(`categories.${router?.query?.category?.toString()}.title.singular`) as string)
-    : undefined;
+  const category = router?.query?.category as Categories;
+  const entryId = router?.query?.entry as string;
 
-  return <AppWrapper titleBar={<TitleBar title={title} />}>TBD</AppWrapper>;
+  const categoryStructure = categories[category];
+  const apiCallFactory = categoryStructure?.api.show.factory;
+  const apiCallRoute = categoryStructure?.api.show.route;
+
+  const { data } = useSWR(
+    apiCallRoute ? getApiUrlString(apiCallRoute, { id: entryId }) : undefined,
+    () => (categoryStructure ? call(apiCallFactory, { id: entryId }) : undefined)
+  );
+
+  const title = (data?.body as any)?.data?.attributes?.name;
+
+  return (
+    <AppWrapper titleBar={<TitleBar title={title} />}>
+      <EntryContainer>
+        <EntryHead>
+          <EntryTitle>{title}</EntryTitle>
+          <EntryDescription>
+            Placeholder: Erat elit mauris rhoncus purus ac in risus. Felis, orci leo viverra enim,
+            nunc, dolor amet, risus orci. Consectetur lacus libero.
+          </EntryDescription>
+        </EntryHead>
+        <EntryContent>
+          <div>Data:</div>
+          <pre>{JSON.stringify(data?.body, null, 2)}</pre>
+        </EntryContent>
+      </EntryContainer>
+    </AppWrapper>
+  );
 };
 
 export default EntryIndexPage;
