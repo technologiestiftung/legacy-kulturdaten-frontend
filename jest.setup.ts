74517f2369fec4db2b334fa7e40e176aa4e4805e
Mathies Schneider <hello@mathies.io>
2021-05-18 17:30:21 +0200

    ✅ Mock uid helper for testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,7 @@ import { publicRuntimeConfig } from './next.config';
 
 // Make sure you can use "publicRuntimeConfig" within tests.
 setConfig({ publicRuntimeConfig });
+
+jest.mock('./lib/uid.ts', () => ({
+  usePseudoUID: () => '111111',
+}));
