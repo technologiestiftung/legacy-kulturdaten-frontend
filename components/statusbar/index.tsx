da8a25fdd5ca28977cc96fbadb3aff7918efc251
Boris Fruendt <hey@boris.io>
2021-05-25 16:32:53 +0200

    :sparkles: Add default value

--- components/statusbar/index.tsx
+++ components/statusbar/index.tsx
@@ -1,22 +1,10 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { SerializedStyles } from '@emotion/utils';
 import { Breakpoint } from '../../lib/WindowService';
 import { mq } from '../globals/Constants';
 import React, { ChangeEvent, useState } from 'react';
 import * as feather from 'react-feather';
 
 import { Label, StyledLabel } from '../label';
-
-export enum StatusBarSize {
-  default = 'default',
-  big = 'big',
-}
-
-export enum StatusBarVariant {
-  default = 'default',
-  minimal = 'minimal',
-}
 
 export enum StatusBarState {
   draft = 'draft',
@@ -29,62 +17,14 @@ const statusBarStates: {
   };
 } = {
   draft: () => ({
-    backgroundColor: 'var(--green-light)',
-  }),
-  published: () => ({
     backgroundColor: 'var(--yellow)',
   }),
-};
-
-const statusBarSizes: {
-  [key in StatusBarSize]: () => {
-    fontSize: string;
-    lineHeight: string;
-    padding: string;
-  };
-} = {
-  default: () => ({
-    fontSize: 'var(--font-size-400)',
-    lineHeight: 'var(--line-height-400)',
-    padding: `0.375rem 3.375rem 0.375rem 0.75rem`,
-  }),
-  big: () => ({
-    fontSize: 'var(--font-size-400)',
-    lineHeight: 'var(--line-height-400)',
-    padding: `0.75rem 3.75rem 0.75rem 0.75rem`,
+  published: () => ({
+    backgroundColor: 'var(--green-light)',
   }),
 };
 
-const statusBarVariants: { [key in StatusBarVariant]: SerializedStyles } = {
-  default: css`
-    background: var(--white);
-    transition: box-shadow var(--transition-duration);
-    box-shadow: var(--shadow);
-
-    &:hover {
-      box-shadow: var(--shadow-hover), inset 0px 0px 0px 1px var(--black);
-    }
-
-    &:active {
-      box-shadow: var(--shadow-active);
-    }
-  `,
-  minimal: css`
-    background: inherit;
-    color: currentColor;
-    border-color: currentColor;
-    transition: box-shadow var(--transition-duration);
-
-    &:hover {
-      box-shadow: inset 0px 0px 0px 1px currentColor;
-    }
-  `,
-};
-
-const StyledStatusBarContainer = styled.div<{
-  statusBarSize: StatusBarSize;
-  variant: StatusBarVariant;
-}>`
+const StyledStatusBarContainer = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
@@ -98,33 +38,29 @@ const StyledStatusBarContainer = styled.div<{
   }
 
   ${StyledLabel} {
-    font-size: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().fontSize};
-    line-height: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().lineHeight};
-    padding: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().padding};
-    padding-right: 0.75rem;
+    font-size: var(--font-size-400);
+    line-height: var(--line-height-400);
+    padding: 0.375rem 0.75rem 0.375rem 0.75rem;
     text-transform: none;
     border-width: 0;
-    
+    background-color: var(--grey-200);
+
     ${mq(Breakpoint.mid)} {
       flex-direction: row;
       border: 1px solid var(--black);
       border-radius: 0.75rem 0 0 0.75rem;
     }
   }
-
-  ${({ variant }) => statusBarVariants[variant]}
 `;
 
-const StyledStatusBarInfo = styled.div<{ 
-  statusBarSize: StatusBarSize, 
-  variant: StatusBarVariant 
-}>`
+const StyledStatusBarInfo = styled.div`
   border: 1px solid var(--black);
   border-right-width: 0;
   border-left-width: 0;
-  font-size: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().fontSize};
-  line-height: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().lineHeight};
-  padding: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().padding};
+  background-color: var(--grey-200);
+  font-size: var(--font-size-400);
+  line-height: var(--line-height-400);
+  padding: 0.375rem 0.75rem 0.375rem 0.75rem;
   position: relative;
   padding-right: 0.75rem;
   flex-grow: 1;
@@ -132,20 +68,16 @@ const StyledStatusBarInfo = styled.div<{
   ${mq(Breakpoint.mid)} {
     text-align: right;
   }
-
-  ${({ variant }) => statusBarVariants[variant]}
 `;
 
-const StyledSelect = styled.select<{ 
-  statusBarSize: StatusBarSize, 
-  variant: StatusBarVariant,
-  statusBarState: StatusBarState
+const StyledSelect = styled.select<{
+  statusBarState: StatusBarState;
 }>`
   margin: 0;
   appearance: none;
-  font-size: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().fontSize};
-  line-height: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().lineHeight};
-  padding: ${({ statusBarSize }) => statusBarSizes[statusBarSize]().padding};
+  font-size: var(--font-size-400);
+  line-height: var(--line-height-400);
+  padding: 0.375rem 3.375rem 0.375rem 0.75rem;
   position: relative;
   width: 100%;
   cursor: pointer;
@@ -159,17 +91,15 @@ const StyledSelect = styled.select<{
     border-radius: 0 0.75rem 0.75rem 0;
   }
 
-  ${({ variant }) => statusBarVariants[variant]}
   ${({ statusBarState }) => statusBarStates[statusBarState]}
 `;
 
-const StyledSelectChevron = styled.div<{ size: StatusBarSize }>`
+const StyledSelectChevron = styled.div`
   pointer-events: none;
   position: absolute;
   left: 0.75rem;
   top: 0;
-  height: ${({ size }) =>
-    size === StatusBarSize.default ? 'calc(2.25rem + 2px)' : 'calc(3rem + 2px)'};
+  height: 2.25rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -186,11 +116,6 @@ const StyledSelectAndChevron = styled.div`
   position: relative;
 `;
 
-const statusBarSizeIconSizeMap: { [key in StatusBarSize]: number } = {
-  default: 18,
-  big: 24,
-};
-
 const statusBarStateMap: { [key in StatusBarState]: string } = {
   draft: 'draft',
   published: 'published',
@@ -200,13 +125,11 @@ interface StatusBarProps {
   children: React.ReactNode;
   id: string;
   value?: string;
-  state?: StatusBarState;
+  statusBarState?: StatusBarState;
   onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
   label?: string;
   info?: string;
   defaultValue?: string;
-  size?: StatusBarSize;
-  variant?: StatusBarVariant;
   ariaLabel?: string;
 }
 
@@ -214,36 +137,26 @@ export const StatusBar: React.FC<StatusBarProps> = ({
   children,
   id,
   value,
+  statusBarState,
   onChange,
   label,
   info,
   defaultValue,
-  size = StatusBarSize.default,
-  variant = StatusBarVariant.default,
   ariaLabel,
 }: StatusBarProps) => {
   const internalState = useState<string>(defaultValue);
   const valueState = value || internalState[0];
 
   return (
-    <StyledStatusBarContainer
-      variant={variant}
-      statusBarSize={size}
-    >
+    <StyledStatusBarContainer>
       {label && <Label htmlFor={id}>{label}</Label>}
-      <StyledStatusBarInfo
-        variant={variant}
-        statusBarSize={size}
-      >{info}
-      </StyledStatusBarInfo>
+      <StyledStatusBarInfo>{info}</StyledStatusBarInfo>
       <StyledSelectAndChevron>
         <StyledSelect
-          variant={variant}
-          statusBarSize={size}
           aria-label={ariaLabel}
           id={id}
           value={valueState}
-          statusBarState={statusBarStateMap[valueState]}
+          statusBarState={valueState as StatusBarState}
           onChange={
             onChange
               ? (e) => onChange(e)
@@ -252,8 +165,8 @@ export const StatusBar: React.FC<StatusBarProps> = ({
         >
           {children}
         </StyledSelect>
-        <StyledSelectChevron size={size}>
-          {React.createElement(feather.ChevronDown, { size: statusBarSizeIconSizeMap[size] })}
+        <StyledSelectChevron>
+          {React.createElement(feather.ChevronDown, { size: 18 })}
         </StyledSelectChevron>
       </StyledSelectAndChevron>
     </StyledStatusBarContainer>
