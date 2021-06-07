4d0aa86be88c7e92a9cbd69a7a0ae503d04ed6aa
Boris Fruendt <hey@boris.io>
2021-06-07 07:58:37 +0200

    :bento: Add tooltip specific assets

---
+++ components/assets/InfoIconSvg.tsx
@@ -0,0 +1,18 @@
+export const InfoIconSvg: React.FC = () => (
+  <svg width="2" height="10" viewBox="0 0 2 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1 8.33333V5"
+      stroke="#111111"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M1 1.66663H1.00667"
+      stroke="#111111"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ components/assets/TooltipArrowMiddleSvg.tsx
@@ -0,0 +1,14 @@
+export const TooltipArrowMiddleSvg: React.FC = () => (
+  <svg width="48px" height="25px" viewBox="0 0 48 25" xmlns="http://www.w3.org/2000/svg">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g id="Rectangle-172">
+        <path
+          d="M36,12 C36,5.3726 30.627,0 24,0 C17.373,0 12,5.3726 12,12 C12,18.6274 6.627,24 -3.97903932e-13,24 L-3.97903932e-13,24.5 L48,24 C41.373,24 36,18.6274 36,12 Z"
+          id="Path"
+          fill="#C8C8C8"
+          fillRule="nonzero"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
