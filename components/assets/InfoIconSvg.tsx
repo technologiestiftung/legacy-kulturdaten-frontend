23bb24f71d4231025f587ba40a0aa20cdb20a956
Boris Fruendt <hey@boris.io>
2021-06-11 09:45:28 +0200

    :bento: Update svg sizes

--- components/assets/InfoIconSvg.tsx
+++ components/assets/InfoIconSvg.tsx
@@ -1,14 +1,14 @@
 export const InfoIconSvg: React.FC = () => (
-  <svg width="2" height="10" viewBox="0 0 2 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M1 8.33333V5"
+      d="M10 13.3333V10"
       stroke="#111111"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M1 1.66663H1.00667"
+      d="M10 6.66663H10.0067"
       stroke="#111111"
       strokeWidth="2"
       strokeLinecap="round"
