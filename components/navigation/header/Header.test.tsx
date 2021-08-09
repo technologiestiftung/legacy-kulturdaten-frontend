73f41b6f47943da4b3a3736455164caf103e2f24
Mathies Schneider <hello@mathies.io>
2021-08-09 16:56:50 +0200

    ✅ Update Header test

--- components/navigation/header/Header.test.tsx
+++ components/navigation/header/Header.test.tsx
@@ -1,8 +1,20 @@
 import { render } from '../../../lib/test-utils';
-import { Header } from './Header';
+import { MenuItemType } from '../Menu';
+import { HeaderMain } from './Header';
 import { HeaderLink } from './HeaderLink';
 
 test('Header renders', () => {
-  const { container } = render(<Header title="Test title" Link={HeaderLink} />);
+  const { container } = render(
+    <HeaderMain
+      title="Test title"
+      Link={HeaderLink}
+      menuItems={[
+        { type: MenuItemType.link, action: { href: '#', title: 'Test' } },
+        { type: MenuItemType.divider },
+      ]}
+      user={undefined}
+      userIsLoggedIn={false}
+    />
+  );
   expect(container).toMatchSnapshot();
 });
