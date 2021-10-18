18de23a7aea12777ebec818d37e6165693af2f3c
Mathies Schneider <hello@mathies.io>
2021-10-18 11:02:49 +0200

    🐛 Remove old attribute from RadioList story

--- components/Radio/story.tsx
+++ components/Radio/story.tsx
@@ -25,7 +25,6 @@ export const RadioListStory: Story = () => {
       <RadioList
         value={value}
         onChange={(newValue) => setValue(newValue)}
-        label="A list of radios"
         name="test-radio"
         options={[
           {
