9803653654fc9560516a3294341c29f0857e34bb
Boris Fruendt <hey@boris.io>
2021-10-19 15:25:41 +0200

    :wrench: Update a11y config

--- config/accessibility.ts
+++ config/accessibility.ts
@@ -13,7 +13,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
       },
       {
         attributes: {
-          language: Language.de,
+          language: Language.en,
           name: 'Planning & arrival',
         },
       },
@@ -29,7 +29,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
           },
           {
             attributes: {
-              language: Language.de,
+              language: Language.en,
               name: 'Who answers questions about accessibility?',
             },
           },
@@ -38,7 +38,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
           {
             type: AccessibilityFieldType.input,
             data: {
-              key: 'planning.serviceContact',
+              key: 'planning.serviceAddress.serviceContact',
               type: InputType.text,
             },
             translations: [
@@ -50,7 +50,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
               },
               {
                 attributes: {
-                  language: Language.de,
+                  language: Language.en,
                   name: 'Name / title',
                 },
               },
@@ -59,7 +59,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
           {
             type: AccessibilityFieldType.input,
             data: {
-              key: 'planning.servicePhoneNumber',
+              key: 'planning.serviceAddress.servicePhoneNumber',
               type: InputType.tel,
             },
             translations: [
@@ -71,7 +71,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
               },
               {
                 attributes: {
-                  language: Language.de,
+                  language: Language.en,
                   name: 'Phone',
                 },
               },
@@ -80,7 +80,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
           {
             type: AccessibilityFieldType.input,
             data: {
-              key: 'planning.serviceMail',
+              key: 'planning.serviceAddress.serviceMail',
               type: InputType.email,
             },
             translations: [
@@ -92,7 +92,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
               },
               {
                 attributes: {
-                  language: Language.de,
+                  language: Language.en,
                   name: 'Email',
                 },
               },
@@ -101,7 +101,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
           {
             type: AccessibilityFieldType.input,
             data: {
-              key: 'planning.serviceHours',
+              key: 'planning.serviceAddress.serviceHours',
               type: InputType.text,
             },
             translations: [
@@ -113,7 +113,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
               },
               {
                 attributes: {
-                  language: Language.de,
+                  language: Language.en,
                   name: 'Office hours',
                 },
               },
@@ -136,7 +136,7 @@ export const locationAccessibility: AccessibilityCategory[] = [
               },
             ],
             data: {
-              key: 'planning.bookingMode',
+              key: 'planning.admissionInfo.bookingMode',
               options: [
                 {
                   value: 'online',
@@ -210,23 +210,239 @@ export const locationAccessibility: AccessibilityCategory[] = [
             },
           },
           {
-            type: AccessibilityFieldType.radioList,
+            type: AccessibilityFieldType.select,
             translations: [
               {
                 attributes: {
                   language: Language.de,
-                  name: 'Bieten die regulären Parkplätze vor Ort genug Platz für Rollstühle?',
+                  name: 'Ermäßigungen',
                 },
               },
               {
                 attributes: {
                   language: Language.en,
-                  name: 'Do the regular parking spaces on site offer enough space for wheelchair users?',
+                  name: 'Reduced admission',
                 },
               },
             ],
             data: {
-              key: 'planning.parking',
+              key: 'planning.admissionInfo.admissionAccessibility',
+              options: [
+                {
+                  value: 'impairmentVisitors',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Ermäßigung für Menschen mit Behinderung / Beeinträchtigung',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Reduced admission for visitors with disability / impairment',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'impairmentAccompany',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Ermäßigungen für Begleitpersonen',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name:
+                          'Discounted admission for attendant accompanying a visitor with a disability / impairment',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'noLibraryCard',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Zugang für Begleitperson auch ohne Nutzer:innenausweis',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Free library card / no library card needed for attendant',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            type: AccessibilityFieldType.input,
+            data: {
+              key: 'planning.admissionInfo.admissionLink',
+              type: InputType.url,
+            },
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name:
+                    'Details zu Eintritt / Tickets für Menschen mit Behinderung / Beeinträchtigung:',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name: 'Details on admission / tickets for people with disabilities:',
+                },
+              },
+            ],
+          },
+          {
+            type: AccessibilityFieldType.input,
+            data: {
+              key: 'planning.arrivalInfo.accessOPNV',
+              type: InputType.text,
+            },
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name:
+                    'Anreise - Ob per ÖPNV, per Auto oder auf anderem Weg: Was muss man wissen, wenn man zu euch kommt?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name:
+                    'Getting there - Whether by public transport, by car or by other means: What is there to know for people traveling to you?',
+                },
+              },
+            ],
+          },
+          {
+            type: AccessibilityFieldType.checkboxList,
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'Wie ist die Haltestelle ausgestattet?:',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'How is the public transport stop equipped?',
+                },
+              },
+            ],
+            data: {
+              key: 'planning.arrivalInfo.accessOPNVinfo',
+              options: [
+                {
+                  value: 'elevator',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Mit Aufzug',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'With elevator',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'wheelchairRamp',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Mit Rollstuhlrampe',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'With ramp for wheelchairs',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'tactilePaving',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Mit Blindenleitsystem',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'With tactile paving',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            type: AccessibilityFieldType.input,
+            data: {
+              key: 'planning.arrivalInfo.accessDirections',
+              type: InputType.text,
+            },
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name:
+                    'Was wäre der beste Weg von der Haltestelle zum Gebäude für Menschen mit Behinderung / Beeinträchtigung?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name:
+                    'What would be the best route from the public transport stop to the building for people with disabilities?',
+                },
+              },
+            ],
+          },
+          {
+            type: AccessibilityFieldType.radioList,
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'Besteht die Möglichkeit, am Eingang zu halten, z.B. für Taxidienste?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name:
+                    'Is there a possibility to stop close to the entrance, e.g. for taxi services?',
+                },
+              },
+            ],
+            data: {
+              key: 'planning.arrivalInfo.accessTaxi',
               options: [
                 {
                   value: 'yes',
@@ -265,6 +481,233 @@ export const locationAccessibility: AccessibilityCategory[] = [
               ],
             },
           },
+          {
+            type: AccessibilityFieldType.radioList,
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name:
+                    'Parken - Gibt es ausgewiesene Parkplätze für Menschen mit Behinderung / Beeinträchtigung?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name:
+                    'Parking - Are there dedicated parking spaces for people with disabilities?',
+                },
+              },
+            ],
+            data: {
+              key: 'planning.arrivalInfo.hasDedicatedParking',
+              options: [
+                {
+                  value: 'yes',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Ja',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Yes',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'no',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Nein',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'No',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            type: AccessibilityFieldType.checkboxList,
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'Gibt es bestimmte Nutzungsbedingungen?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name: 'Is a registration or special parking permitt required?',
+                },
+              },
+            ],
+            data: {
+              key: 'planning.arrivalInfo.neededParkingPermits',
+              options: [
+                {
+                  value: 'reservation',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Voranmeldung',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Parking space needs to be reserved in advance',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'blueCard',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Blauer EU-Parkausweis',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Blue EU disability parking card required',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'orangeCard',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Oranger Ausweis über Parkerleichterung',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: "Orange 'Ausweis über Parkerleichterung'",
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'disabledPass',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Schwerbehindertenausweis',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: "'Schwerbehindertenausweis' required",
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            type: AccessibilityFieldType.radioList,
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'Bieten die regulären Parkplätze vor Ort genug Platz für Rollstühle?',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name:
+                    'Do the regular parking spaces on site offer enough space for wheelchair users?',
+                },
+              },
+            ],
+            data: {
+              key: 'planning.arrivalInfo.spaceForWheelchair',
+              options: [
+                {
+                  value: 'yes',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Ja',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'Yes',
+                      },
+                    },
+                  ],
+                },
+                {
+                  value: 'no',
+                  translations: [
+                    {
+                      attributes: {
+                        language: Language.de,
+                        name: 'Nein',
+                      },
+                    },
+                    {
+                      attributes: {
+                        language: Language.en,
+                        name: 'No',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            type: AccessibilityFieldType.input,
+            data: {
+              key: 'planning.arrivalInfo.parkingInfo',
+              type: InputType.text,
+            },
+            translations: [
+              {
+                attributes: {
+                  language: Language.de,
+                  name: 'Anmerkung',
+                },
+              },
+              {
+                attributes: {
+                  language: Language.en,
+                  name: 'A note on parking',
+                },
+              },
+            ],
+          },
+
           {
             type: AccessibilityFieldType.checkboxList,
             translations: [
