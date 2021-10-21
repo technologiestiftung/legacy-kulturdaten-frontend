cac60f2afcc08e7c6f71521a3305ab4c9f8173ff
Mathies Schneider <hello@mathies.io>
2021-10-21 18:20:06 +0200

    ✨ Advance DropdownMenu with text

--- components/DropdownMenu/index.tsx
+++ components/DropdownMenu/index.tsx
@@ -5,16 +5,18 @@ import * as feather from 'react-feather';
 import { Breakpoint } from '../../lib/WindowService';
 import { mq } from '../globals/Constants';
 
-const StyledDropdownMenu = styled.div``;
+const StyledDropdownMenu = styled.div`
+  position: relative;
+`;
 
-const StyledDropdownMenuButton = styled.button<{ visible: boolean }>`
+const StyledDropdownMenuButton = styled.button<{ visible: boolean; form?: DropdownMenuForm }>`
   appearance: none;
   border: none;
   background: var(--black);
-  display: block;
+  display: flex;
   color: var(--white);
-  padding: 0.5625rem;
-  border-radius: 2rem;
+  column-gap: 0.75rem;
+  border-radius: ${({ form }) => (form === DropdownMenuForm.round ? '2rem' : '0.75rem')};
   margin: 0;
   line-height: 0;
   box-shadow: 0.125rem 0.125rem 0.75rem -0.125rem rgba(0, 0, 0, 0.4);
@@ -23,6 +25,7 @@ const StyledDropdownMenuButton = styled.button<{ visible: boolean }>`
   transition: transform var(--transition-duration);
   position: relative;
   z-index: 2;
+  padding: 0;
 
   ${({ visible }) =>
     visible
@@ -34,6 +37,18 @@ const StyledDropdownMenuButton = styled.button<{ visible: boolean }>`
           }
         `
       : ''}
+`;
+
+const StyledDropdownMenuButtonText = styled.div`
+  font-size: var(--font-size-300);
+  line-height: var(--line-height-300);
+  font-weight: 700;
+  padding: 0.375rem 0 0.375rem 1.125rem;
+`;
+
+const StyledDropdownMenuButtonIcon = styled.div<{ hasText?: boolean }>`
+  line-height: 0;
+  padding: ${({ hasText }) => (hasText ? '0.5625rem 1.125rem 0.5625rem 0' : '0.5625rem')};
 
   svg {
     width: 1.125rem;
@@ -41,21 +56,25 @@ const StyledDropdownMenuButton = styled.button<{ visible: boolean }>`
   }
 `;
 
-const StyledDropdownMenuDropdown = styled.div<{ visible: boolean; animating: boolean }>`
+const StyledDropdownMenuDropdown = styled.div<{
+  visible: boolean;
+  animating: boolean;
+}>`
   background: var(--grey-200);
+  border-radius: 1.125rem;
   border-radius: 1.125rem;
   width: calc(var(--app-width) - 1.5rem);
   position: absolute;
-  right: 0.75rem;
+  right: 0;
   transition: opacity var(--transition-duration), transform var(--transition-duration);
   transform: translate(0rem, -1rem);
   opacity: 0;
   box-shadow: 0.125rem 0.125rem 3rem -0.25rem rgba(0, 0, 0, 0.5);
   display: none;
   visibility: hidden;
+  z-index: 1;
 
   ${mq(Breakpoint.mid)} {
-    right: 1.5rem;
     width: auto;
     min-width: 18rem;
   }
@@ -96,6 +115,11 @@ export const DropdownMenuText = styled.div`
   word-break: break-all;
 `;
 
+export enum DropdownMenuForm {
+  round = 'round',
+  rounded = 'rounded',
+}
+
 interface DropdownMenuProps {
   children: React.ReactNode;
   icon: string;
@@ -103,16 +127,21 @@ interface DropdownMenuProps {
     open: string;
     close: string;
   };
+  text?: string;
+  form?: DropdownMenuForm;
 }
 
 export const DropdownMenu: React.FC<DropdownMenuProps> = ({
   children,
   icon,
+  text,
   buttonAriaLabels,
+  form = DropdownMenuForm.round,
 }: DropdownMenuProps) => {
   const [visible, setVisible] = useState(false);
   const [animating, setAnimating] = useState(false);
   const dropdownMenuRef = useRef<HTMLDivElement>(null);
+  const hasText = typeof text !== 'undefined';
 
   const clickHandler = useMemo(
     () => (manualVisible?: boolean) => {
@@ -151,8 +180,14 @@ export const DropdownMenu: React.FC<DropdownMenuProps> = ({
         onClick={() => clickHandler()}
         visible={visible}
         aria-label={visible ? buttonAriaLabels.close : buttonAriaLabels.open}
+        form={form}
       >
-        {visible ? React.createElement(feather.X) : React.createElement(feather[icon || 'Circle'])}
+        {text && <StyledDropdownMenuButtonText>{text}</StyledDropdownMenuButtonText>}
+        <StyledDropdownMenuButtonIcon hasText={hasText}>
+          {visible
+            ? React.createElement(feather.X)
+            : React.createElement(feather[icon || 'Circle'])}
+        </StyledDropdownMenuButtonIcon>
       </StyledDropdownMenuButton>
       <StyledDropdownMenuDropdown visible={visible} animating={animating}>
         <StyledDropdownMenuDropdownContent>{children}</StyledDropdownMenuDropdownContent>
