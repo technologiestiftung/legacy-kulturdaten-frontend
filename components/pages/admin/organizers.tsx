7574078c332eadc6c92953dcfb585976aff25139
Mathies Schneider <hello@mathies.io>
2021-11-12 13:06:05 +0100

    ✨ Redirect non superuser admin away from admin area

--- components/pages/admin/organizers.tsx
+++ components/pages/admin/organizers.tsx
@@ -1,8 +1,25 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { routes } from '../../../config/routes';
 import { useT } from '../../../lib/i18n';
+import { useLocale } from '../../../lib/routing';
+import { useOrganizerId } from '../../../lib/useOrganizer';
 import { EntryHeader } from '../../EntryHeader';
+import { useUser } from '../../user/useUser';
 
 export const AdminOrganizersPage: React.FC = () => {
   const t = useT();
+  const { user, isSuperuser } = useUser();
+  const router = useRouter();
+  const locale = useLocale();
+  const organizerId = useOrganizerId();
+
+  // Redirect non superuser users to dashboard
+  useEffect(() => {
+    if (user?.id && !isSuperuser) {
+      router.replace(routes.dashboard({ locale, query: { organizer: organizerId } }));
+    }
+  }, [isSuperuser, locale, organizerId, router, user?.id]);
 
   return (
     <>
