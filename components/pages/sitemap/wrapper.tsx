663748a283b496da53ac6c4ebd6e8b3691b5274a
Urs Schmidt <urs.schmidt@ts.berlin>
2022-08-15 10:17:48 +0200

    KDB-235 offers and locations in sitemap

--- components/pages/sitemap/wrapper.tsx
+++ components/pages/sitemap/wrapper.tsx
@@ -3,20 +3,24 @@ import { defaultLanguage } from "../../../config/locale";
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import { useT } from "../../../lib/i18n";
-import { useLanguage } from "../../../lib/routing"
+import { routes, Routes, useLanguage, useLocale } from "../../../lib/routing"
 import { getTranslation } from "../../../lib/translations";
 import { useHandleActiveOrganizer, useOrganizerId } from "../../../lib/useOrganizer";
 import { EntryFormContainer, EntryFormWrapper } from "../../EntryForm/wrappers";
 import { EntryHeader } from "../../EntryHeader"
 import { useUser, useUserOrganizerLists } from "../../user/useUser";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { appLayouts, useLayout } from "../../layouts/AppLayout";
 import { useMenuStructure } from "../../../config/structure";
-import { useCategories } from "../../../config/categories";
+import { Categories, useCategories } from "../../../config/categories";
 import { OfferList } from "../../../lib/api";
-import { Offer } from "../../../lib/api/types/offer";
+import { Offer, OfferTranslation, OfferTypeTranslation } from "../../../lib/api/types/offer";
 import { useList } from "../../../lib/categories";
 import { defaultOrganizerId } from "../../navigation/NavigationContext";
+import { LocationTranslation } from "../../../lib/api/types/location";
+import router from "next/router";
+import { PublishedStatus } from "../../../lib/api/types/general";
+import { EntryCard, EntryCardTypesSubjects } from "../../EntryList/EntryCard";
 
 const StyledLinkList = styled.ul`
 
@@ -37,50 +41,141 @@ export const SitemapContainer: React.FC<SitemapContainerProps> = ({}: SitemapCon
   const organizerLists = useUserOrganizerLists();
   const organizerId = useOrganizerId();
   const categories = useCategories();
+  const [lastPage, setLastPage] = useState<number>();
+  const [totalEntries, setTotalEntries] = useState<number>();
+  const locale = useLocale();
 
   const NavigationStructure = useMenuStructure();
 
-  const offers = useList<OfferList, Offer>(
+  const offerList = useList<OfferList, Offer>(
     categories.offer,
-    1,
-    1,
+    lastPage,
+    totalEntries,
     [['organizers', organizerId]],
     undefined,
     organizerId !== defaultOrganizerId
   );
 
-  const locations = useList<OfferList, Offer>(
+  const locationList = useList<OfferList, Offer>(
     categories.location,
-    1,
-    1,
+    lastPage,
+    totalEntries,
     [['organizer', organizerId]],
     undefined,
     organizerId !== defaultOrganizerId
   );
 
-  const hasOffers = useMemo(() => offers?.data?.length > 0, [offers]);
-  const hasLocations = useMemo(() => locations?.data?.length > 0, [locations]);
+
+  const offers = useMemo(
+    () =>
+      offerList?.data
+        ? Object.values(Array.isArray(offerList.data) ? offerList.data : [offerList.data]).map(
+            ({ attributes, relations, id }) => {
+
+              const href = (sub?: string) =>
+                routes[Routes.offer]({
+                  locale,
+                  query: { organizer: organizerId, id, sub },
+                });
+
+              const translations = relations?.translations;
+
+              const currentTranslation = translations
+                ? getTranslation<OfferTranslation>(language, translations)
+                : undefined;
+
+              const defaultTranslation = translations
+                ? getTranslation<OfferTranslation>(defaultLanguage, translations)
+                : undefined;
+
+              const translation = 
+                currentTranslation.attributes.name ?
+                currentTranslation.attributes.name :
+                defaultTranslation.attributes.name ?
+                defaultTranslation.attributes.name :
+                t('general.placeholderOffer')
+
+              return {
+                href: href('info'),
+                translation
+              }
+            }
+          )
+        : undefined,
+    [
+      language,
+      offerList.data,
+      locale,
+      organizerId,
+      t
+    ]
+  );
+
+
+  const locations = useMemo(
+    () =>
+      locationList?.data
+        ? Object.values(Array.isArray(locationList.data) ? locationList.data : [locationList.data]).map(
+            ({ attributes, relations, id }) => {
+
+              const href = (sub?: string) =>
+                routes[Routes.offer]({
+                  locale,
+                  query: { organizer: organizerId, id, sub },
+                });
+
+              const translations = relations?.translations;
+
+              const currentTranslation = translations
+                ? getTranslation<OfferTranslation>(language, translations)
+                : undefined;
+
+              const defaultTranslation = translations
+                ? getTranslation<OfferTranslation>(defaultLanguage, translations)
+                : undefined;
+
+              const translation = 
+                currentTranslation.attributes.name ?
+                currentTranslation.attributes.name :
+                defaultTranslation.attributes.name ?
+                defaultTranslation.attributes.name :
+                t('general.placeholderLocation')
+
+              return {
+                href: href('info'),
+                translation
+              }
+            }
+          )
+        : undefined,
+    [
+      language,
+      locationList.data,
+      locale,
+      organizerId,
+      t
+    ]
+  );
+
 
   const level_1_links = useMemo(() => {return NavigationStructure.header.loggedIn.menuItems.filter(item => item.type === 'link')}, [ NavigationStructure ])
   const level_2_links = useMemo(() => {return NavigationStructure.menus}, [ NavigationStructure ])
-  const organizer = organizerLists.all.filter(organizer => organizer.id === organizerId)[0] || undefined
   
-  const organizerHandler = useHandleActiveOrganizer()
-  console.log("LINKs",organizerHandler)
+  const organizer = organizerLists.all.filter(organizer => organizer.id === organizerId)[0] || undefined
 
-  const currentTranslation = organizer ? getTranslation(language, organizer.relations?.translations, true) : undefined
-  const defaultTranslation = organizer ? getTranslation(defaultLanguage, organizer.relations?.translations, true) : undefined
+  const organizerCurrentTranslation = organizer ? getTranslation(language, organizer.relations?.translations, true) : undefined
+  const organizerDefaultTranslation = organizer ? getTranslation(defaultLanguage, organizer.relations?.translations, true) : undefined
 
-  const subTitle = 
-    currentTranslation?.attributes?.name ||
-    defaultTranslation?.attributes?.name ||
+  const organizerSubTitle = 
+    organizerCurrentTranslation?.attributes?.name ||
+    organizerDefaultTranslation?.attributes?.name ||
     (t('general.placeholderOrganizer') as string)
 
   return (
     <>
       <EntryHeader 
         title="Sitemap"
-        subTitle={subTitle}
+        subTitle={organizerSubTitle}
       />
       <EntryFormWrapper>
         <EntryFormContainer>
@@ -88,6 +183,16 @@ export const SitemapContainer: React.FC<SitemapContainerProps> = ({}: SitemapCon
             {level_1_links.map((topic, index) => (
               <StyledLink level={0} key={index}>
                 <Link href={topic.action.href}>{topic.action.title || ''}</Link>
+                {index === 1 && offers && offers.map((offer, index) => (
+                  <StyledLink key={index} level={1}>
+                    <Link href={offer?.href as string}>{offer.translation}</Link>
+                  </StyledLink>
+                ))}
+                {index === 2 && locations && locations.map((offer, index) => (
+                  <StyledLink key={index} level={1}>
+                    <Link href={offer?.href as string}>{offer.translation}</Link>
+                  </StyledLink>
+                ))}
               </StyledLink>
             ))}
             
